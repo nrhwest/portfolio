d4770a5cf8c4f9e973bb4dff005d357296225495
Nathan West <nathanwest@Nathan-Ws-MacBook-Pro.local>
2019-01-29 17:20:32 -0500

    menu bar and separate pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main'
+import { Link } from 'react-router-dom';
+
+
+/*
+  Tutorial used to help create site : https://www.youtube.com/watch?v=n8iA18R76jk&t=395s
+*/
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="demo-big-content">
+        <Layout>
+          <Header className="header-color" title="Title" scroll>
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/about_me">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/about_me">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
+        </Layout>
       </div>
     );
   }
---
+++ src/components/landing_page.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div><h1>Landing page</h1></div>
+
+            // <div style={{ width: '100%', margin: 'auto' }}>
+            //     <Grid className="landing_grid">
+            //         <Cell col={12}></Cell>
+            //     </Grid>
+            // </div>
+        );
+    }
+}
+
+export default LandingPage;
