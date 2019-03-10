fb0b6b83ee70cd2ecc363440eee1a070300a6f3f
nrhwest <nrhwest@gmail.com>
2019-03-10 16:22:31 -0400

    updates to contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,7 +11,9 @@ class Contact extends Component {
                         <img 
                             src={require('../images/nathan1.jpg')}
                             alt="hi"
-                            className="nathan-img" /> 
+                            className="nathan-img" 
+                            style={{height: '250px'}}/> 
+                        <p style={{width: '75%', margin: 'auto'}}>I can always be reached via email, cell phone, or Linkedin</p>
                     </Cell>
                     <Cell col={6}>gang</Cell>
                 </Grid>
